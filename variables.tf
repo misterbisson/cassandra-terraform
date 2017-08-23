d6983fe44b2765bc8c8cf3e9a72052feb2a6417d
Brig Young <brig@sonophotostudios.com>
2017-08-23 16:16:49 -0700

    variablizing instance_type, default t2.micro

--- variables.tf
+++ variables.tf
@@ -3,12 +3,14 @@ variable "access_key" {}
 variable "secret_key" {}
 variable "ssh_key_path" {}
 variable "ssh_key_name" {}
+variable "instance_type" {
+  default = "t2.micro"
+}
 variable "vpc_id" {}
 variable "security_group_name" {}
 
-# cannot do interolations in varables.
-# default source_cidr_block is the main subnet
-# defined in aws_subnet.main.cidr_block.
+# We cannot do interpolations in varables.
+# Default source_cidr_block is the main subnet defined in aws_subnet.main.cidr_block.
 variable "source_cidr_block" {
   default = "172.31.32.0/20"
 }
